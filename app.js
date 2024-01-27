f98d2a08b91562e2c13918c3a0f16d81ce585087
MDe <demi_05@arcor.de>
2024-01-27 12:00:27 +0100

    Funktioniert, gibt immer ein JSON-Dokument zurueck

--- app.js
+++ app.js
@@ -2,48 +2,85 @@ const express = require("express");
 
 const app = express();
 
+const HTTP_STATUS_BAD_REQUEST = 400;
+const HTTP_STATUS_OK          = 200;
+
 /*
  * REST-Endpunkt für Berechnung Body-Mass-Index (BMI).
  * URL-Parameter:
  * * kg: Gewicht in kg
  * * cm: Körpergröße in cm
  *
- * Beispiel-URL:
+ * Beispiel-URL für Aufruf:
  * http://localhost:8080/bmi?kg=80&cm=180
  *
- * siehe auch:
+ * Rückgabe:
+ * * HTTP-Status-Code 400 (Bad Request) bei fehlerhaften URL-Parametern
+ * * HTTP-Status-Code 200 (OK) bei erfolgreicher Berechnung
+ *
+ * Erklärung BMI:
  * https://www.barmer.de/gesundheit-verstehen/leben/abnehmen-diaet/body-mass-index/bmi-rechner-1004244
  */
 app.get("/bmi", (req, res) => {
 
-    // URL-Parameter abfragen
+    // überprüfen, ob URL-Parameter vorhanden sind
     const kgStr = req.query.kg;
     const cmStr = req.query.cm;
 
-    let kg = -1;
-    try {
-        kg = parseInt(kgStr);
+    if (kgStr === undefined) {
+
+        res.status(HTTP_STATUS_BAD_REQUEST)
+           .json({ erfolg   : false,
+                   nachricht: "Bitte URL-Parameter >kg< angeben" })
+        return;
     }
-    catch (e) {
-        console.log(`Fehler beim Parsen von URL-Parameter kg=${kgStr}: ${e}`);
-        res.send("Bitte Zahl für URL-Parameter >kg< angeben");
+    if (cmStr === undefined) {
+
+        res.status(HTTP_STATUS_BAD_REQUEST)
+            .json({ erfolg   : false,
+                    nachricht: "Bitte URL-Parameter >cm< angeben" })
         return;
     }
 
-    let cm = -1;
-    try {
-        cm = parseInt(cmStr);
+    const kg = parseInt(kgStr);
+    if (isNaN(kg)) {
+
+        res.status(HTTP_STATUS_BAD_REQUEST)
+           .json({ erfolg   : false,
+                   nachricht: `Wert "${kgStr}" für URL-Parameter >kg< ist keine int-Zahl.` });
+        return;
     }
-    catch (e) {
-        console.log(`Fehler beim Parsen von URL-Parameter cm=${cmStr}: ${e}`);
-        res.send("Bitte Zahl für URL-Parameter >cm< angeben");
+    const cm = parseInt(cmStr);
+    if (isNaN(cm)) {
+
+        res.status(HTTP_STATUS_BAD_REQUEST)
+           .json({ erfolg   : false,
+                   nachricht: `Wert "${cmStr}" für URL-Parameter >cm< ist keine int-Zahl.` });
         return;
     }
 
-    res.send("Noch nicht implementiert :-(");
+    // eigentliche Berechnung
+    let bmi = kg / (cm * cm) * 10000; // *10.000 wegen cm statt m
+
+    // auf eine Nachkommastelle runden
+    bmi = Math.round(bmi * 10) / 10;
+
+    let interpretation = "";
+    if      (bmi < 18.5) { interpretation = "Untergewicht";        }
+    else if (bmi < 25.0) { interpretation = "Normalgewicht";       }
+    else if (bmi < 30.0) { interpretation = "Prä-Adipositas";      }
+    else if (bmi < 35.0) { interpretation = "Moderate Adipositas"; }
+    else if (bmi < 40.0) { interpretation = "Starke Adipositas";   }
+    else { interpretation = "Extreme Adipositas"; }
+
+    res.status(HTTP_STATUS_OK)
+       .json({ erfolg   : true,
+               bmi      : bmi,
+               nachricht: interpretation });
+
 });
 
 const portNummer = 8080;
-app.listen( 8080,
-            () => { console.log(`Web-Server lauscht auf Port ${8080}`); }
+app.listen( portNummer,
+            () => { console.log(`Web-Server lauscht auf Port ${portNummer}`); }
           );

e3c70422cbf3e9e71d5f22e34a00fc3f0598d6fe
MDecker-MobileComputing <mide_42b@yahoo.de>
2025-02-09 09:59:00 +0100

    Neue Version von express.js in package.json, kleine Verbesserungen in mehreren Dateien

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const PORT_NUMMER = 8080;
 const HTTP_STATUS_BAD_REQUEST = 400;
 const HTTP_STATUS_OK          = 200;
 
+
 /*
  * REST-Endpunkt für Berechnung Body-Mass-Index (BMI).
  * URL-Parameter:
@@ -23,59 +24,59 @@ const HTTP_STATUS_OK          = 200;
  * Erklärung "Body Mass Index" (BMI) und Interpretation der Werte, siehe z.B. hier:
  * https://www.barmer.de/gesundheit-verstehen/leben/abnehmen-diaet/body-mass-index/bmi-rechner-1004244
  */
-app.get("/bmi", (request, response) => {
+app.get( "/bmi", ( request, response ) => {
 
     // überprüfen, ob URL-Parameter vorhanden sind
     const kgStr = request.query.kg;
     const cmStr = request.query.cm;
 
-    if (kgStr === undefined) {
+    if ( kgStr === undefined ) {
 
-        response.status(HTTP_STATUS_BAD_REQUEST)
+        response.status( HTTP_STATUS_BAD_REQUEST )
            .json({ erfolg   : false,
                    nachricht: "Bitte URL-Parameter >kg< angeben." });
         return;
     }
     if (cmStr === undefined) {
 
-        response.status(HTTP_STATUS_BAD_REQUEST)
+        response.status( HTTP_STATUS_BAD_REQUEST )
             .json({ erfolg   : false,
                     nachricht: "Bitte URL-Parameter >cm< angeben." });
         return;
     }
 
-    const kg = parseInt(kgStr);
-    if (isNaN(kg)) {
+    const kg = parseInt( kgStr );
+    if ( isNaN( kg ) ) {
 
-        response.status(HTTP_STATUS_BAD_REQUEST)
+        response.status( HTTP_STATUS_BAD_REQUEST )
            .json({ erfolg   : false,
                    nachricht: `Wert "${kgStr}" für URL-Parameter >kg< ist keine Zahl.` });
         return;
     }
-    const cm = parseInt(cmStr);
-    if (isNaN(cm)) {
+    const cm = parseInt( cmStr );
+    if ( isNaN( cm ) ) {
 
-        response.status(HTTP_STATUS_BAD_REQUEST)
+        response.status( HTTP_STATUS_BAD_REQUEST )
            .json({ erfolg   : false,
                    nachricht: `Wert "${cmStr}" für URL-Parameter >cm< ist keine Zahl.` });
         return;
     }
 
     // eigentliche Berechnung
-    let bmi = kg / (cm * cm) * 10000; // *10.000 wegen cm statt m
+    let bmi = kg / ( cm * cm ) * 10000; // *10.000 wegen cm statt m
 
     // auf eine Nachkommastelle runden
-    bmi = Math.round(bmi * 10) / 10;
+    bmi = Math.round( bmi * 10 ) / 10;
 
     let interpretation = "";
-    if      (bmi < 18.5) { interpretation = "Untergewicht";        }
-    else if (bmi < 25.0) { interpretation = "Normalgewicht";       }
-    else if (bmi < 30.0) { interpretation = "Prä-Adipositas";      }
-    else if (bmi < 35.0) { interpretation = "Moderate Adipositas"; }
-    else if (bmi < 40.0) { interpretation = "Starke Adipositas";   }
-    else                 { interpretation = "Extreme Adipositas";  }
+    if      ( bmi < 18.5 ) { interpretation = "Untergewicht";        }
+    else if ( bmi < 25.0 ) { interpretation = "Normalgewicht";       }
+    else if ( bmi < 30.0 ) { interpretation = "Prä-Adipositas";      }
+    else if ( bmi < 35.0 ) { interpretation = "Moderate Adipositas"; }
+    else if ( bmi < 40.0 ) { interpretation = "Starke Adipositas";   }
+    else                   { interpretation = "Extreme Adipositas";  }
 
-    response.status(HTTP_STATUS_OK)
+    response.status( HTTP_STATUS_OK )
        .json({ erfolg   : true,
                bmi      : bmi,
                nachricht: interpretation });
@@ -83,7 +84,7 @@ app.get("/bmi", (request, response) => {
 
 
 // statische Dateien (z.B. "index.html") aus Unterordner "public/" bereitstellen
-app.use( express.static("public") );
+app.use( express.static( "public" ) );
 
 // Web-Server starten
 app.listen( PORT_NUMMER,
